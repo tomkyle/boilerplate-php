ea9a7d0ec2edfcde0c3693c14c21658513dc5721
Carsten Witt <412560+tomkyle@users.noreply.github.com>
2022-05-09 11:47:51 +0200

    Add Package header

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,11 +1,22 @@
 <?php
+$header = <<<EOF
+PHP Package Boilerplate (https://github.com/tomkyle/boilerplate-php)
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+EOF;
 
 $finder = PhpCsFixer\Finder::create()->in([
-        __DIR__ . '/src',
-        __DIR__ . '/tests/src'
-    ]);
+    __DIR__ . '/src',
+    __DIR__ . '/tests/src'
+]);
 
-$config = new PhpCsFixer\Config();
-return $config->setRules([
-    '@PSR12' => true
+return (new PhpCsFixer\Config())->setRules([
+    '@PSR12' => true,
+    'header_comment' => [
+        'comment_type' => 'PHPDoc',
+        'header' => $header,
+        'location' => 'after_open',
+        'separate' => 'both',
+    ]
 ])->setFinder($finder);
--- tests/src/LoggerTrait.php
+++ tests/src/LoggerTrait.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * PHP Package Boilerplate (https://github.com/tomkyle/boilerplate-php)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace tests;
 
 use Psr;
