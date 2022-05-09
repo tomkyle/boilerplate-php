086ee2cb5605b4a22f4e45651374020b9f8456fe
tomkyle <412560+tomkyle@users.noreply.github.com>
2022-05-09 14:12:48 +0200

    Simplify

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,10 +6,9 @@ For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
 EOF;
 
-$finder = PhpCsFixer\Finder::create()->in([
-    __DIR__ . '/src',
-    __DIR__ . '/tests/src'
-]);
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/')
+    ->exclude(__DIR__ . '/vendor');
 
 return (new PhpCsFixer\Config())->setRules([
     '@PSR12' => true,
